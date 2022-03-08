eea5d6cd03778b7d0d164bd9c552a925c398332a
medfahmy <medfahmy99@gmail.com>
2022-03-08 09:58:16 +0100

    turnstile fsm

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,12 @@
 use std::io::{self, BufRead, Write};
 
-#[derive(Debug, Clone, Copy)]
-enum State {
-    Locked,
-    Unlocked,
-}
-
-#[derive(Debug)]
-enum Event {
-    Push,
-    Coin,
-}
-
-use State::*;
-use Event::*;
-
-const STATES_COUNT: usize = 2;
-const EVENTS_COUNT: usize = 2;
-
-const FSM: [[State; EVENTS_COUNT]; STATES_COUNT] = [
-    [Locked, Unlocked],
-    [Locked, Unlocked]
-];
-
-// fn next_state(state: State, event: Event) -> State {
-//     match event {
-//         Push => Locked,
-//         Coin => Unlocked,
-//     }
-// }
-
-fn next_state(state: State, event: Event) -> State {
-    FSM[state as usize][event as usize]
-}
-
+mod turnstile;
+use turnstile::*;
 
 fn main() {
-    let mut state = Locked;
+    println!("hi mom");
+
+    let mut state = State::Locked;
 
     // let arr = [1, 2, 4];
     // println!("{}", arr[state as usize]);
@@ -44,17 +14,20 @@ fn main() {
     println!("State: {:?}", state);
 
     print!("> ");
-    io::stdout().flush();
-    for line in io::stdin().lock().lines() {
-        match line.unwrap().as_str() {
-            "push" => state = next_state(state, Push),
-            "coin" => state = next_state(state, Coin),
-            "quit" => return,
-            unknown => eprintln!("ERROR: unknown event {}", unknown)
-        }
+    match io::stdout().flush() {
+        _result => {
+            for line in io::stdin().lock().lines() {
+                match line.unwrap().as_str() {
+                    "push" => state = next_state(state, Event::Push),
+                    "coin" => state = turnstile::next_state(state, Event::Coin),
+                    "quit" => return,
+                    unknown => eprintln!("ERROR: unknown event {}", unknown)
+                }
 
-        println!("{:?}", state);
-        print!("> ");
-        io::stdout().flush().unwrap();
+                println!("{:?}", state);
+                print!("> ");
+                io::stdout().flush().unwrap();
+            }
+        }
     }
 }
---
+++ src/turnstile.rs
@@ -0,0 +1,34 @@
+
+#[derive(Debug, Clone, Copy)]
+pub enum State {
+    Locked,
+    Unlocked,
+}
+
+#[derive(Debug)]
+pub enum Event {
+    Push,
+    Coin,
+}
+
+use State::*;
+use Event::*;
+
+const STATES_COUNT: usize = 2;
+const EVENTS_COUNT: usize = 2;
+
+const FSM: [[State; EVENTS_COUNT]; STATES_COUNT] = [
+    [Locked, Unlocked],
+    [Locked, Unlocked]
+];
+
+// fn next_state(state: State, event: Event) -> State {
+//     match event {
+//         Push => Locked,
+//         Coin => Unlocked,
+//     }
+// }
+
+pub fn next_state(state: State, event: Event) -> State {
+    FSM[state as usize][event as usize]
+}
