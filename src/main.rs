952b8c30e7188cf7b301ee904a31e4230dab1773
medfahmy <medfahmy99@gmail.com>
2022-03-10 12:43:55 +0100

    range

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+
+use std::ops::Range;
+
 mod turnstile;
 
 type FsmIndex = usize;
@@ -6,14 +12,15 @@ struct FsmColumn {
     ts: [FsmIndex; 127],
 }
 
-
 impl FsmColumn {
     fn new() -> Self {
         FsmColumn { ts: [0; 127] }
     }
 
-    fn f(range: Range<usize>, state: FsmIndex) {
-
+    fn fill_range(&mut self, range: Range<char>, state: FsmIndex) {
+        for i in range.map(|x| x as usize) {
+            self.ts[i] = state;
+        }
     }
 }
 
@@ -21,7 +28,15 @@ type Fsm = Vec<FsmColumn>;
 
 fn main() {
     let mut fsm = Fsm::new();
-    fsm.push(FsmColumn::new());
+
+    let mut col = FsmColumn::new();
+
+    // col.fill_range();
+    // fsm.push();
 
     // turnstile::play();
+
+    println!("{:?}", ('a'..'z').map(|x| x as u8).collect::<Vec<u8>>());
+
+    // println!("{:?}", vec!['a', 'b', 'c'].into_iter().map(|c| c as u8).rev().collect::<Vec<u8>>());
 }
